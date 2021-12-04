d54a612ee9f4d4bf2c91c798f3b08ab356586ec2
George Montgomery <george@happsio.com>
2021-12-04 14:10:53 -0500

    part 2

---
+++ src/2021/day_3/part_2/main.rs
@@ -0,0 +1,62 @@
+fn main() {
+    let split = advent_of_code::INPUT.split("\n");
+    let vec: Vec<&str> = split.collect();
+    let mut majorities: Vec<&str> = vec.clone();
+    let mut minorities: Vec<&str> = vec.clone();
+    let mut index = 0;
+
+    while majorities.len() > 1 {
+        // println!("{:?}", majorities.len());
+        majorities = meeting_criteria_at_index(index, majorities, Criteria::Majority);
+        index = index + 1;
+    }
+    index = 0;
+    // println!("{:?}", majorities.len());
+    let oxygen = isize::from_str_radix(&*majorities[0], 2).unwrap();
+
+    while minorities.len() > 1 {
+        // println!("{:?}", minorities.len());
+        minorities = meeting_criteria_at_index(index, minorities, Criteria::Minority);
+        index = index + 1;
+    }
+    // println!("{:?}", minorities.len());
+    let c_oh_two = isize::from_str_radix(&*minorities[0], 2).unwrap();
+    // println!("{:?}", c_oh_two);
+
+    println!("{:?}", c_oh_two * oxygen);
+}
+
+enum Criteria {
+    Majority,
+    Minority,
+}
+
+fn meeting_criteria_at_index(index: i32, list: Vec<&str>, criteria: Criteria) -> Vec<&str> {
+    let list_size = list.len();
+    let mut ones_count = 0;
+    for line in list.iter() {
+        if line.chars().nth(index as usize).unwrap() == '1' {
+            ones_count = ones_count + 1
+        }
+    }
+    let more_or_equal_ones = ones_count >= (list_size - ones_count);
+    let keep_char = match criteria {
+        Criteria::Majority => {
+            if more_or_equal_ones {
+                '1'
+            } else {
+                '0'
+            }
+        }
+        Criteria::Minority => {
+            if more_or_equal_ones {
+                '0'
+            } else {
+                '1'
+            }
+        }
+    };
+    list.into_iter()
+        .filter(|&string| string.chars().nth(index as usize).unwrap() == keep_char)
+        .collect::<Vec<&str>>()
+}
